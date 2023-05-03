df36c293b4202a4fbdaaceac2f8ccdc3998a67ca
Kazuaki Tanaka <kazuaki@ics.kyutech.ac.jp>
2023-05-03 17:04:46 +0900

    コンパイラのバージョンを選択できるようにした

--- mrbc/app/main.rb
+++ mrbc/app/main.rb
@@ -7,62 +7,66 @@ require 'open3'
 set :bind, '0.0.0.0'
 
 get '/' do
-  "working..."
+  "It works."
 end
 
-# TODO: バージョン情報を返す機能を実装する
+
+# TODO: バージョン情報を環境から取得する
+def versions
+  ["2.0.1", "3.2.0"]
+end
+
 get '/versions' do
     content_type :json
-    a = ["2.0.1", "3.2.0"]
-    a.to_json
+    versions.to_json
 end
 
 # 動作確認用のエディタ
 get "/editor" do
-    erb :editor
+  @versions = versions
+  erb :editor
 end
  
-# TODO: コンパイル部分を実装する
 post '/compile' do
-    name = params[:name]
-    program = params[:program]
-    version = params[:version]
+  name = params[:name]
+  program = params[:program]
+  version = params[:version]
   
-    mrbc_path = ""
-    if version=="3.2.0" then
-      mrbc_path = "/bin/mrbc3.2.0"
-    elsif version=="2.0.1" then
-      mrbc_path = "/bin/mrbc2.0.1"
+  mrbc_path = ""
+  if version=="3.2.0" then
+    mrbc_path = "/bin/mrbc3.2.0"
+  elsif version=="2.0.1" then
+    mrbc_path = "/bin/mrbc2.0.1"
+  else
+    mrbc_path = "/bin/mrbc3.2.0"    # デフォルトのコンパイラ
+  end
+  
+  # プログラム名が指定されなかった場合
+  if name=="" then
+    name = "default"
+  end
+  
+  Tempfile.create([name, '.rb']) do |f|
+    #プログラムをtmpファイルに保存
+    path = f.path
+    fname = File.basename(path, '.*')
+    f.puts program
+    f.rewind
+    
+    #tmpファイルのコンパイル
+    cpcmd = "#{mrbc_path} #{path}"
+    puts cpcmd
+    cpr, cpe, cps = Open3.capture3(cpcmd)
+    #cpr:標準出力, cpe:標準エラー, cps:プロセス終了ステータス
+    
+    if cpe.empty? then
+      mrbpath = "/tmp/#{fname}.mrb"
+      send_file(mrbpath, filename: "#{name}.mrb") 
+      
     else
-      mrbc_path = "/bin/mrbc3.2.0"    # デフォルトのコンパイラ
+      puts "Error"
+      erb :error
     end
-
-    # プログラム名が指定されなかった場合
-    if name=="" then
-        name = "default"
-    end
-
-    Tempfile.create([name, '.rb']) do |f|
-      #プログラムをtmpファイルに保存
-      path = f.path
-      fname = File.basename(path, '.*')
-      f.puts program
-      f.rewind
-  
-      #tmpファイルのコンパイル
-      cpcmd = "#{mrbc_path} #{path}"
-      puts cpcmd
-      cpr, cpe, cps = Open3.capture3(cpcmd)
-      #cpr:標準出力, cpe:標準エラー, cps:プロセス終了ステータス
-  
-      if cpe.empty? then
-        mrbpath = "/tmp/#{fname}.mrb"
-        send_file(mrbpath, filename: "#{name}.mrb") 
-  
-      else
-        puts "Error"
-        erb :error
-      end
-  
-    end
+    
+  end
 end
