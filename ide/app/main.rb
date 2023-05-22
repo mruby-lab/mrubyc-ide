e9a8b03e51311e6607a6df90e38c3ae920774bc8
Kazuaki Tanaka <kazuaki@ics.kyutech.ac.jp>
2023-05-22 21:27:48 +0900

    ダウンロード、BLEの処理を切り替えるようにした   /editor でダウンロード用   /editor/senstick でSenStick用

--- ide/app/main.rb
+++ ide/app/main.rb
@@ -11,6 +11,41 @@ end
 
 # エディタ
 get "/editor" do
+  @writer = :download
+  @writer_name = :download
+  @writer_action = "/compile"
+  erb :editor
+end
+  
+
+get "/editor/:writer" do
+  case params['writer']
+  when 'ble' then
+    @writer = :ble
+    @writer_name = "BLE"
+    @writer_action = "/send_ble"
+  when 'senstick' then
+    @writer = :ble
+    @writer_name = "SenStick"
+    @writer_action = "/send_ble"
+  else
+    @writer = :download
+    @writer_name = :download
+    @writer_action = "/compile"
+  end
   erb :editor
 end
  
+
+# コンパイル＆ダウンロードの処理
+# mrbcコンテナのサービスを使う
+post "/compile" do
+  "/compile"
+end
+
+
+# BLE転送の処理
+post "/send_ble" do
+  "/send_ble"
+end
+
