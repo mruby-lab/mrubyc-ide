b13eb97320a0ceab655b4bcff1eefa3a74bfc6e4
Kazuaki Tanaka <kaz0505.tanaka@gmail.com>
2023-07-17 22:34:48 +0900

    APIのURLを修正した、SSL対応

--- ide/app/main.rb
+++ ide/app/main.rb
@@ -9,10 +9,10 @@ set :server_settings,
     SSLEnable: true,
     SSLCertName: [['CN', WEBrick::Utils.getservername]]
 
-    
+
 #$api = 'http://host.docker.internal:4566'
 #上記URLは開発環境用
-$api = 'http://mrubyc-ide.ddns.net:4566'
+$api = 'https://mrubyc-ide.ddns.net:4566'
 
 set :bind, '0.0.0.0'
 set :port, 4567
